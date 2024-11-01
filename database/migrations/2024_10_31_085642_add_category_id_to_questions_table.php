d26f4c0bdbf39e6a4f58afcf06b3e9a4de0f400d
RaihanPrasetia <raihanprasetia86@gmail.com>
2024-11-01 16:12:30 +0700

    membuat category_id null

--- database/migrations/2024_10_31_085642_add_category_id_to_questions_table.php
+++ database/migrations/2024_10_31_085642_add_category_id_to_questions_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('questions', function (Blueprint $table) {
-            $table->foreignId('category_id')->constrained('categories')->after('survey_id');
+            $table->foreignId('category_id')->nullable()->constrained('categories')->after('survey_id');
         });
     }
 
